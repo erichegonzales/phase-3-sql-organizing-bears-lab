7b11337e4eeb02afd10ea51e733eb54f1cb76725
Eriche Gonzales <erichelouise@gmail.com>
2022-08-15 23:05:01 -0400

    commit tree 5d875555a333a1dc2f1c1b2bc9f451056080bc7e

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, 'Mr. Chocolate', 12, 'M', 'Brown', 'Mild', 1);
+INSERT INTO bears VALUES (2, 'Rowdy', 1, 'M', 'Black', 'Mild', 1);
+INSERT INTO bears VALUES (3, 'Tabitha', 2, 'F', 'Cream', 'Nice', 1);
+INSERT INTO bears VALUES (4, 'Sergeant Brown', 19, 'M', 'Brown', 'Angry', 1);
+INSERT INTO bears VALUES (5, 'Melissa', 15, 'F', 'Orange', 'Mild', 1);
+INSERT INTO bears VALUES (6, 'Grinch', 3, 'M', 'Tan', 'Angry', 1);
+INSERT INTO bears VALUES (7, 'Wendy', 7, 'F', 'White', 'Nice', 1);
+INSERT INTO bears VALUES (8, NULL, 5, 'F', 'Brown', 'Mild', 1);
